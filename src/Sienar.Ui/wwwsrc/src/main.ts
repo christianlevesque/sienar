1dbce383303e00963341473df7e38df12a2a3355
speegs92 <christian@levesque.dev>
2026-07-20 17:53:50 -0500

    removed Sienar.Ui project

--- src/Sienar.Ui/wwwsrc/src/main.ts
+++
@@ -1,6 +0,0 @@
-import '@mdi/font/fonts/materialdesignicons-webfont.eot';
-import '@mdi/font/fonts/materialdesignicons-webfont.ttf';
-import '@mdi/font/fonts/materialdesignicons-webfont.woff';
-import '@mdi/font/fonts/materialdesignicons-webfont.woff2';
-import '@mdi/font/css/materialdesignicons.min.css';
-import '../styles/index.scss';
