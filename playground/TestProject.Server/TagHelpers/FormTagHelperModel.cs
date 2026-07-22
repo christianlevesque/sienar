9ab24b9c16ffd7712b8cd294b26806f089873013
speegs92 <christian@levesque.dev>
2026-07-22 13:46:21 -0500

    saving work

--- playground/TestProject.Server/TagHelpers/FormTagHelperModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Razor.TagHelpers;
-
-namespace TestProject.TagHelpers;
-
-public class FormTagHelperModel
-{
-	public required TagHelperOutput InputHtml { get; set; }
-	public required TagHelperOutput LabelHtml { get; set; }
-}
